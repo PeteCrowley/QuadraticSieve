0dd9a1cd6071ff7a0301c50a15f78c36131624c7
Pete Crowley <pc207@duke.edu>
2025-04-17 00:26:17 -0400

    better intervals + parallel

--- quadratic_sieve.py
+++ quadratic_sieve.py
@@ -5,7 +5,13 @@ import sys
 import time
 from galois import GF2
 import math
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import os
 
+
+EXTRA_VECTORS = 10
+SIEVE_INTERVAL_SIZE = 1_000_000
+PRIMES_TO_SKIP = 10
 
 
 def choose_factor_base_bound(N) -> int:
@@ -33,7 +39,10 @@ def b_smooth_factor(x: int, factor_base: list) -> dict | None:
         :returns powers: A dictionary of the form {p: exponent} if x is B-smooth, None otherwise
     """
     powers = dict()
-    for p in factor_base:
+    if x < 0:
+        powers[-1] = 1
+        x = -x
+    for p in factor_base[1:]:
         while x % p == 0:
             if p not in powers:
                 powers[p] = 1
@@ -53,16 +62,82 @@ def build_smart_factor_base(N, B) -> list:
         :param B: The bound for the factor base
         :returns factor_base: A list of primes that are quadratic residues mod N
     """
-    factor_base = []
+    factor_base = [-1]
     for p in primerange(2, B+1):
         if is_quadratic_residue(N, p):
             factor_base.append(p)
     return factor_base
 
-#TODO: Improve this method a lot
-# Specificaly: 1) change sieve intervals to all be around sqrt(kN) for some k instead of just going up from sqrt(N)
-# 2) Expand the search to an interval sqrt(kn) - A to sqrt(kn) + A for some A instead of just above sqrt(N)
-# 3) Parallelize this search to make it much quicker
+
+def sieve_and_filter(N, B, factor_base, dict_factor_base, log_factor_base, iteration):
+    """The exact same functionality as the better_find_b_smooth_squares function, but parallelized"""
+    def f(x):
+        return x**2 - N
+
+    sieve_interval_center = math.ceil(math.sqrt(N))
+    small_start = sieve_interval_center - (iteration+1) * SIEVE_INTERVAL_SIZE
+    large_start = sieve_interval_center + iteration * SIEVE_INTERVAL_SIZE
+    smaller_registers = np.zeros(SIEVE_INTERVAL_SIZE)     # checking below sqrt(N) will have [sieve_center + center_distance, sieve_center + center_distance + SIEVE_INTERVAL_SIZE)
+    larger_registers = np.zeros(SIEVE_INTERVAL_SIZE)     # checking above sqrt(N) will have [sieve_center - center_distance + SIEVE_INTERVAL_SIZE, sieve_center - center_distance + SIEVE_INTERVAL_SIZE)]
+
+    for p in factor_base[PRIMES_TO_SKIP:]:
+        sols = sqrt_mod(N, p, all_roots=True)
+        for sol in sols:
+            for i in range((sol-large_start) % p, SIEVE_INTERVAL_SIZE, p):
+                larger_registers[i] += log_factor_base[dict_factor_base[p]]
+            for i in range((sol-small_start) % p, SIEVE_INTERVAL_SIZE, p):
+                smaller_registers[i] += log_factor_base[dict_factor_base[p]]
+
+    results = []
+    for x in range(SIEVE_INTERVAL_SIZE):
+        for start, regs in [(small_start, smaller_registers), (large_start, larger_registers)]:
+            if regs[x] >= math.log(x+start) - math.log(B):
+                factors = b_smooth_factor(f(x+start), factor_base)
+                if factors is not None:
+                    
+                    results.append((x+start, factors))
+    return results
+   
+def parallel_find_b_smooth_squares(N, B, factor_base, dict_factor_base) -> list:
+    """Just a parallel version of the better_find_b_smooth_squares function"""
+    log_factor_base  = [math.log(p) if p != -1 else 0 for p in factor_base]
+    nums = []
+    factorizations = []
+
+    max_iters = (int(math.sqrt(N)) // SIEVE_INTERVAL_SIZE) - 1
+    vectors_needed = len(factor_base) + 1 + EXTRA_VECTORS
+
+    start_number = os.cpu_count()   # let's just start with the number of iterations our computer can do in parallel
+
+    with ProcessPoolExecutor() as executor:
+        futures = []
+        # submit the first start_number iterations
+        for i in range(0, start_number):
+            futures.append(executor.submit(sieve_and_filter, N, B, factor_base, dict_factor_base, log_factor_base, i))
+        iteration = start_number
+
+        while futures and len(nums) < vectors_needed:
+            for future in as_completed(futures):
+                result = future.result()
+                for x, factors in result:
+                    nums.append(x)
+                    factorizations.append(factors)
+
+                futures.remove(future)      # remove the iteration we just finished
+
+                # if we have more work to do, submit the next iteration
+                if iteration < max_iters and len(nums) < vectors_needed:
+                    futures.append(executor.submit(sieve_and_filter, N, B, factor_base, dict_factor_base, log_factor_base, iteration))
+                    iteration += 1
+
+                # if we have enough numbers, break
+                if len(nums) >= vectors_needed:
+                    break
+                
+    return nums, factorizations
+
+#TODO: Improve this method
+# Specificaly: 1) change sieve intervals to all be around sqrt(kN) for some k instead of just looking around sqrt(N)
 def better_find_b_smooth_squares(N, B, factor_base, dict_factor_base) -> list:
     """Finds B-smooth squares mod N using the quadratic sieve method
 
@@ -76,41 +151,48 @@ def better_find_b_smooth_squares(N, B, factor_base, dict_factor_base) -> list:
     def f(x):
         return x**2 - N
     
-    log_factor_base  = np.log(factor_base)
+    log_factor_base  = [math.log(p) if p != -1 else 0 for p in factor_base]
 
-    sieve_interval = 100_000
-    sieve_start = math.ceil(math.sqrt(N))
+    sieve_interval_center = math.ceil(math.sqrt(N))
+    iteration = 0
 
     nums = []
     factorizations = []
 
     # we want enough numbers to guarantee a linear dependence
-    while len(nums) < len(factor_base) + 5:
-        registers = [0] * (sieve_interval)     # check from s to s + A, initialize to log of the number
+    while len(nums) < len(factor_base) + 1 + EXTRA_VECTORS:
+        small_start = sieve_interval_center - (iteration+1) * SIEVE_INTERVAL_SIZE
+        large_start = sieve_interval_center + iteration * SIEVE_INTERVAL_SIZE
 
-
-        for p in factor_base:
+        smaller_registers = np.zeros(SIEVE_INTERVAL_SIZE)     # checking below sqrt(N) will have [sieve_center + center_distance, sieve_center + center_distance + SIEVE_INTERVAL_SIZE)
+        larger_registers = np.zeros(SIEVE_INTERVAL_SIZE)     # checking above sqrt(N) will have [sieve_center - center_distance + SIEVE_INTERVAL_SIZE, sieve_center - center_distance + SIEVE_INTERVAL_SIZE)]
+        
+        for p in factor_base[1+PRIMES_TO_SKIP:]:
             sols = sqrt_mod(N, p, all_roots=True)   # find the two solutions (if they exist)
             for sol in sols:
-                for i in range((sol-sieve_start) % p, sieve_interval, p):   # all numbers congruent to sol mod p are also solutions
-                    registers[i] += log_factor_base[dict_factor_base[p]]    # so we add the log of the prime to the register
 
+                for i in range((sol-large_start) % p, SIEVE_INTERVAL_SIZE, p):   # all numbers congruent to sol mod p are also solutions
+                    larger_registers[i] += log_factor_base[dict_factor_base[p]]    # so we add the log of the prime to the register
+
+                for i in range((sol-small_start) % p, SIEVE_INTERVAL_SIZE, p):   # all numbers congruent to sol mod p are also solutions
+                    smaller_registers[i] += log_factor_base[dict_factor_base[p]]    # so we add the log of the prime to the register
         
-
         # for numbers with small values now, we will check if they are B smooth
-        for x in range(0, sieve_interval):
-            if registers[x] >= math.log(x+sieve_start) - math.log(B):
-                factors = b_smooth_factor(f(x+sieve_start), factor_base)
-                if factors is not None:
-                    # if the number is B smooth, we will add it to our list
-                    nums.append(x+sieve_start)
-                    factorizations.append(factors)
-                    # print(len(nums), nums[-1], factorizations[-1])
-                    if len(nums) >= len(factor_base) + 1:
-                        break
+        for x in range(0, SIEVE_INTERVAL_SIZE):
+            for start, regs in [(small_start, smaller_registers), (large_start, larger_registers)]:
+                if regs[x] >= math.log(x+start) - math.log(B):
+                    factors = b_smooth_factor(f(x+start), factor_base)
+                    if factors is not None:
+                        # if the number is B smooth, we will add it to our list
+                        nums.append(x+start)
+                        factorizations.append(factors)
+                        # print(len(nums), nums[-1], factorizations[-1])
+                        if len(nums) >= len(factor_base) + 1 + EXTRA_VECTORS:
+                            break
 
-        sieve_start += sieve_interval
-        if sieve_start > N:
+
+        iteration += 1
+        if (iteration+1) * SIEVE_INTERVAL_SIZE > math.sqrt(N):
             break
         
     return nums, factorizations
@@ -140,14 +222,14 @@ def quadratic_sieve(N, verbose=False):
         :returns factors: A tuple of the two factors if found, None if it can't find any"""
     B = choose_factor_base_bound(N)
     factor_base = list(build_smart_factor_base(N, B))
-    # print(len(factor_base), factor_base)
     if verbose:
         print(f"=== Chose factor base of {B} consisting of {len(factor_base)} primes===")
     dict_factor_base = {p: i for i, p in enumerate(factor_base)}
 
     if verbose:
         print("=== Searching for B-smooth squares ===")
-    nums, factorizations = better_find_b_smooth_squares(N, B, factor_base, dict_factor_base)
+    # nums, factorizations = better_find_b_smooth_squares(N, B, factor_base, dict_factor_base)
+    nums, factorizations = parallel_find_b_smooth_squares(N, B, factor_base, dict_factor_base)
 
     if nums == []:
         return None
@@ -165,7 +247,6 @@ def quadratic_sieve(N, verbose=False):
     # any row vector will work as a linear combo that yields 0 mod 2
     for row in null_space:
         linear_combo = row
-        # print(row)
         # each 1 in this row represents a congruence we should include
         exponents_squared = [0 for _ in factor_base]
         sqrt_number = 1
@@ -199,7 +280,6 @@ def quadratic_sieve(N, verbose=False):
 if __name__ == '__main__':
     # Number to factor
     N = int(sys.argv[1])
-    print(N)
     time_start = time.time()
     quadratic_sieve(N, verbose=True)
     time_end = time.time()
